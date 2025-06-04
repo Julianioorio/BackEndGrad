56091da58fe8dc043068f2fe83a203d078a97167
Makey Julian <makeyjulian@gmail.com>
2025-06-04 22:23:19 +0300

    beta-tonsClick

--- src/app.js
+++ src/app.js
@@ -1,6 +1,33 @@
 const express = require("express");
+const mysql = require('mysql2');
+const cors = require("cors");
 const app = express();
-const port = 3000;
+const port = 4000;
+
+app.use(cors()); 
+app.use(express.json()); 
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'julianMak',
+  database: 'my_database',
+});
+
+db.connect(err => {
+  if (err) {
+    console.error('Ошибка подключения к MySQL:', err.message);
+  } else {
+    console.log('Подключено к MySQL');
+  }
+});
+
+app.get('/products', (req, res) => {
+  db.query('SELECT * FROM products', (err, results) => {
+    if (err) return res.status(500).send('Ошибка БД');
+    res.json(results);
+  });
+});
 
 app.get("/", (req, res) => {
   res.send("Привет!");
